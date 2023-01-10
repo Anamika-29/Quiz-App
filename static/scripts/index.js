f260e975c7d627ed7f842c599f0663563e318378
Anamika-29 <job.anamika29@gmail.com>
2023-01-10 18:33:50 +0530

    questions added

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -31,7 +31,10 @@ const quizData = [
         d: "none of the above",
         correct: "b",
     },
+
+
 ];
+
 const quiz= document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
@@ -40,21 +43,30 @@ const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
+
+
 let currentQuiz = 0
 let score = 0
+
 loadQuiz()
+
 function loadQuiz() {
+
     deselectAnswers()
+
     const currentQuizData = quizData[currentQuiz]
+
     questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
+
 function deselectAnswers() {
     answerEls.forEach(answerEl => answerEl.checked = false)
 }
+
 function getSelected() {
     let answer
     answerEls.forEach(answerEl => {
@@ -64,18 +76,23 @@ function getSelected() {
     })
     return answer
 }
+
+
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
     if(answer) {
        if(answer === quizData[currentQuiz].correct) {
            score++
        }
+
        currentQuiz++
+
        if(currentQuiz < quizData.length) {
            loadQuiz()
        } else {
            quiz.innerHTML = `
            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
            <button onclick="location.reload()">Reload</button>
            `
        }
